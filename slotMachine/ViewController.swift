aaa0ed11d25527bbaa75090e055a7d8cb1a3a277
Mark C. Kosirog <mark.kosirog@vg-mbp-kosirog.local>
2016-06-17 11:03:08 -0500

    Slot machine with Play again.

--- slotMachine/ViewController.swift
+++ slotMachine/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    
+    
+    var slotEmojis = ["👺","😈","👿","👽","👻"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func screenTap(sender: UITapGestureRecognizer) {
+        
+        print("tapped")
+        let randomEmoji1 = Int(arc4random_uniform(UInt32(slotEmojis.count)))
+        let randomEmoji2 = Int(arc4random_uniform(UInt32(slotEmojis.count)))
+        let randomEmoji3 = Int(arc4random_uniform(UInt32(slotEmojis.count)))
+       
+        
+        label1.text = slotEmojis[randomEmoji1]
+        label2.text = slotEmojis[randomEmoji2]
+        label3.text = slotEmojis[randomEmoji3]
+        
+    if label1.text == label2.text && label2.text == label3.text
+        {
+            print("winner")
+            alert ()
+        }
+    }
+    
+    func  alert()
+    {
+        let myAlert = UIAlertController(title: "Jackpot", message: "You win $1000", preferredStyle: UIAlertControllerStyle.Alert)
+        myAlert.addAction(UIAlertAction (title: "Play Again?", style: UIAlertActionStyle.Default, handler: { sender in
+            
+            self.label1.text = self.slotEmojis[0]
+            self.label2.text = self.slotEmojis[0]
+            self.label3.text = self.slotEmojis[0]
+    }))
+        presentViewController(myAlert,animated: true, completion: nil)
+    }
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
